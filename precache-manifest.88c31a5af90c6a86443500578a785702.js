974d1dc329150dc38827b3147a66a8f44bf015f4
PedroZambra <pezamesp@gmail.com>
2019-08-22 00:39:20 +0200

    Updates

---
+++ precache-manifest.88c31a5af90c6a86443500578a785702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1e3843a1c5deae794556abb0ac7faa",
+    "url": "/react-cajero/index.html"
+  },
+  {
+    "revision": "8539104839c6c9987f5c",
+    "url": "/react-cajero/static/css/main.990dd5d3.chunk.css"
+  },
+  {
+    "revision": "df10d223d93d67d221e2",
+    "url": "/react-cajero/static/js/2.8eebee82.chunk.js"
+  },
+  {
+    "revision": "8539104839c6c9987f5c",
+    "url": "/react-cajero/static/js/main.fad64a8a.chunk.js"
+  },
+  {
+    "revision": "1b597b8019ca5226bb89",
+    "url": "/react-cajero/static/js/runtime~main.354b3ea1.js"
+  },
+  {
+    "revision": "56db4f988f7e61257b5309604d24b7b2",
+    "url": "/react-cajero/static/media/credit_card.56db4f98.png"
+  }
+]);
